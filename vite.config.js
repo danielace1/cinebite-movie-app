732521a2b4fc00307e2ada3ad09e69ba792d26e3
danielace1 <danielsudharsan1805@gmail.com>
2025-12-15 17:35:27 +0530

    feat: meta tags, pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,53 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "icons/icon-192.png",
+        "icons/icon-512.png",
+      ],
+
+      manifest: {
+        name: "CineBite",
+        short_name: "CineBite",
+        description: "Discover Movies & TV Shows. Build your watchlist.",
+        theme_color: "#020617",
+        background_color: "#020617",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
+
+        icons: [
+          {
+            src: "/icons/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
